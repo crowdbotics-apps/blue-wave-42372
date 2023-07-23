870a0c0e8eda3f05cae7c6df96659c1145676e48
Crowdbotics <team@crowdbotics.com>
2023-07-23 20:26:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-wave-42372",
+  url: "https://blue-wave-42372.botics.co",
+  api: "https://blue-wave-42372.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
